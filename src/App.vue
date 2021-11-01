1f42bbfc3212722b5d0601d539babb87fb9e90fc
Vince Veselosky <vince@control-escape.com>
2021-11-01 14:11:53 -0400

    Replace vue template boilerplate with my own

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <Masthead />
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
@@ -6,25 +7,14 @@
   <router-view />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import Masthead from "@/components/Masthead.vue";
+export default {
+  name: "App",
+  components: {
+    Masthead,
+  },
+};
+</script>
+<style lang="scss"></style>
---
+++ src/components/Masthead.vue
@@ -0,0 +1,59 @@
+<template>
+  <header class="blog-header py-3">
+    <div class="row flex-nowrap justify-content-between align-items-center">
+      <div class="col-8">
+        <a class="blog-header-logo text-dark" href="/"
+          >{{ site.name }}
+          <span v-if="site.tagline">| {{ site.tagline }}</span></a
+        >
+      </div>
+      <div class="col-4 d-flex justify-content-end align-items-center">
+        <a class="text-muted" href="#">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="mx-3"
+          >
+            <circle cx="10.5" cy="10.5" r="7.5"></circle>
+            <line x1="21" y1="21" x2="15.8" y2="15.8"></line>
+          </svg>
+        </a>
+        <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Masthead",
+  data() {
+    return {
+      site: {
+        name: "Bukkit CMS",
+        tagline: "I has a bukkit!",
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.blog-header {
+  line-height: 1;
+}
+.blog-header-logo {
+  font-family: "Playfair Display", Georgia, "Times New Roman", serif;
+  font-size: 2.25rem;
+}
+.blog-header-logo:hover {
+  text-decoration: none;
+}
+</style>
